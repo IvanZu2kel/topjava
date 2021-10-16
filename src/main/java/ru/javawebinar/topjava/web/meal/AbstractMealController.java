5df5d05130b91858f46d632f4d6e9c830fbdbe10
Ivan Zu2kel <ivan_zukkel@mail.ru>
2021-10-16 18:20:29 +0300

    HW02 4 and 5 tasks are completed

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.service.UserService;
+
+import java.util.Collection;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+public abstract class AbstractMealController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MealService service;
+
+    public Meal get(int id) {
+        log.info("getById {}", id);
+        return service.get(id);
+    }
+
+    public Collection<Meal> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public Meal create(Meal meal) {
+        log.info("create {}", meal);
+        return service.create(meal);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public void update(Meal meal, int id) {
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
+        service.update(meal);
+    }
+}
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,17 +1,35 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
 
 @Controller
-public class MealRestController {
+public class MealRestController extends AbstractMealController{
 
-    private final MealService service;
-
-    @Autowired
-    public MealRestController(MealService service) {
-        this.service = service;
+    @Override
+    public Meal get(int id) {
+        return super.get(id);
     }
 
+    @Override
+    public Collection<Meal> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return super.create(meal);
+    }
+
+    @Override
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    @Override
+    public void update(Meal meal, int id) {
+        super.update(meal, id);
+    }
 }
